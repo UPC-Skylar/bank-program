a95348a7ba60e2fa088eee677cea88badc7586a1
UPC-Skylar <ryvencode@gmail.com>
2026-09-17 00:35:36 -0500

    add main.java

--- src/BalanceCalculator.java
+++ src/BalanceCalculator.java
@@ -1,3 +1,10 @@
 public class BalanceCalculator {
-    
+    public static double deposit(double balance, double amount) {
+        return balance + amount;
+    }
+
+    public static double withdraw(double balance, double amount) {
+        //Por ahora no validamos reglas, solo calculos
+        return balance - amount;
+    }
 }
